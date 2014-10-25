cd8145461017926ef0e800a63d2a218de9477631
475517977@qq.com <qinjun318512>
2014-10-26 03:22:00 +0800

    Revert "Revert "GOOD""
    This reverts commit 356d421ccb644c0f54a430e7a45f28ad25de223c.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace My2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pb16 = new System.Windows.Forms.PictureBox();
+            this.pb15 = new System.Windows.Forms.PictureBox();
+            this.pb14 = new System.Windows.Forms.PictureBox();
+            this.pb13 = new System.Windows.Forms.PictureBox();
+            this.pb12 = new System.Windows.Forms.PictureBox();
+            this.pb11 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.backlabel = new System.Windows.Forms.Label();
+            this.ini_bton = new System.Windows.Forms.Button();
+            this.score = new System.Windows.Forms.Label();
+            this.scorebord = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(143)))), ((int)(((byte)(122)))), ((int)(((byte)(101)))));
+            this.label1.Image = global::My2048.Properties.Resources.score;
+            this.label1.Location = new System.Drawing.Point(42, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 34;
+            // 
+            // pb16
+            // 
+            this.pb16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb16.Image = global::My2048.Properties.Resources._0;
+            this.pb16.Location = new System.Drawing.Point(391, 468);
+            this.pb16.Name = "pb16";
+            this.pb16.Size = new System.Drawing.Size(105, 105);
+            this.pb16.TabIndex = 33;
+            this.pb16.TabStop = false;
+            // 
+            // pb15
+            // 
+            this.pb15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb15.Image = global::My2048.Properties.Resources._0;
+            this.pb15.Location = new System.Drawing.Point(274, 468);
+            this.pb15.Name = "pb15";
+            this.pb15.Size = new System.Drawing.Size(105, 105);
+            this.pb15.TabIndex = 32;
+            this.pb15.TabStop = false;
+            // 
+            // pb14
+            // 
+            this.pb14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb14.Image = global::My2048.Properties.Resources._0;
+            this.pb14.Location = new System.Drawing.Point(158, 468);
+            this.pb14.Name = "pb14";
+            this.pb14.Size = new System.Drawing.Size(105, 105);
+            this.pb14.TabIndex = 31;
+            this.pb14.TabStop = false;
+            // 
+            // pb13
+            // 
+            this.pb13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb13.Image = global::My2048.Properties.Resources._0;
+            this.pb13.Location = new System.Drawing.Point(42, 468);
+            this.pb13.Name = "pb13";
+            this.pb13.Size = new System.Drawing.Size(105, 105);
+            this.pb13.TabIndex = 30;
+            this.pb13.TabStop = false;
+            // 
+            // pb12
+            // 
+            this.pb12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb12.Image = global::My2048.Properties.Resources._0;
+            this.pb12.Location = new System.Drawing.Point(391, 353);
+            this.pb12.Name = "pb12";
+            this.pb12.Size = new System.Drawing.Size(105, 105);
+            this.pb12.TabIndex = 29;
+            this.pb12.TabStop = false;
+            // 
+            // pb11
+            // 
+            this.pb11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb11.Image = global::My2048.Properties.Resources._0;
+            this.pb11.Location = new System.Drawing.Point(274, 353);
+            this.pb11.Name = "pb11";
+            this.pb11.Size = new System.Drawing.Size(105, 105);
+            this.pb11.TabIndex = 28;
+            this.pb11.TabStop = false;
+            // 
+            // pb10
+            // 
+            this.pb10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb10.Image = global::My2048.Properties.Resources._0;
+            this.pb10.Location = new System.Drawing.Point(158, 353);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(105, 105);
+            this.pb10.TabIndex = 27;
+            this.pb10.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb9.Image = global::My2048.Properties.Resources._0;
+            this.pb9.Location = new System.Drawing.Point(42, 353);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(105, 105);
+            this.pb9.TabIndex = 26;
+            this.pb9.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb8.Image = global::My2048.Properties.Resources._0;
+            this.pb8.Location = new System.Drawing.Point(391, 238);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(105, 105);
+            this.pb8.TabIndex = 25;
+            this.pb8.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb7.Image = global::My2048.Properties.Resources._0;
+            this.pb7.Location = new System.Drawing.Point(274, 238);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(105, 105);
+            this.pb7.TabIndex = 24;
+            this.pb7.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb6.Image = global::My2048.Properties.Resources._0;
+            this.pb6.Location = new System.Drawing.Point(158, 238);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(105, 105);
+            this.pb6.TabIndex = 23;
+            this.pb6.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb5.Image = global::My2048.Properties.Resources._0;
+            this.pb5.Location = new System.Drawing.Point(42, 238);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(105, 105);
+            this.pb5.TabIndex = 22;
+            this.pb5.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb4.Image = global::My2048.Properties.Resources._0;
+            this.pb4.Location = new System.Drawing.Point(391, 120);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(105, 105);
+            this.pb4.TabIndex = 21;
+            this.pb4.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb3.Image = global::My2048.Properties.Resources._0;
+            this.pb3.Location = new System.Drawing.Point(274, 120);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(105, 105);
+            this.pb3.TabIndex = 20;
+            this.pb3.TabStop = false;
+            // 
+            // pb2
+            // 
+            this.pb2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb2.Image = global::My2048.Properties.Resources._0;
+            this.pb2.Location = new System.Drawing.Point(158, 120);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(105, 105);
+            this.pb2.TabIndex = 19;
+            this.pb2.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb1.Image = global::My2048.Properties.Resources._0;
+            this.pb1.Location = new System.Drawing.Point(42, 120);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(105, 105);
+            this.pb1.TabIndex = 18;
+            this.pb1.TabStop = false;
+            // 
+            // backlabel
+            // 
+            this.backlabel.Image = global::My2048.Properties.Resources.backlabel;
+            this.backlabel.Location = new System.Drawing.Point(31, 109);
+            this.backlabel.Name = "backlabel";
+            this.backlabel.Size = new System.Drawing.Size(475, 475);
+            this.backlabel.TabIndex = 17;
+            // 
+            // ini_bton
+            // 
+            this.ini_bton.BackgroundImage = global::My2048.Properties.Resources.NewGame;
+            this.ini_bton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ini_bton.FlatAppearance.BorderSize = 0;
+            this.ini_bton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ini_bton.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ini_bton.Location = new System.Drawing.Point(377, 40);
+            this.ini_bton.Name = "ini_bton";
+            this.ini_bton.Size = new System.Drawing.Size(129, 40);
+            this.ini_bton.TabIndex = 16;
+            this.ini_bton.UseVisualStyleBackColor = false;
+            this.ini_bton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // score
+            // 
+            this.score.Image = global::My2048.Properties.Resources.score;
+            this.score.Location = new System.Drawing.Point(31, 40);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(129, 40);
+            this.score.TabIndex = 35;
+            // 
+            // scorebord
+            // 
+            this.scorebord.AutoSize = true;
+            this.scorebord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(143)))), ((int)(((byte)(122)))), ((int)(((byte)(101)))));
+            this.scorebord.Font = new System.Drawing.Font("微软雅黑", 23F);
+            this.scorebord.ForeColor = System.Drawing.Color.White;
+            this.scorebord.Location = new System.Drawing.Point(179, 40);
+            this.scorebord.Name = "scorebord";
+            this.scorebord.Size = new System.Drawing.Size(35, 40);
+            this.scorebord.TabIndex = 36;
+            this.scorebord.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(248)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(539, 604);
+            this.Controls.Add(this.scorebord);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb16);
+            this.Controls.Add(this.pb15);
+            this.Controls.Add(this.pb14);
+            this.Controls.Add(this.pb13);
+            this.Controls.Add(this.pb12);
+            this.Controls.Add(this.pb11);
+            this.Controls.Add(this.pb10);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.backlabel);
+            this.Controls.Add(this.ini_bton);
+            this.MaximumSize = new System.Drawing.Size(555, 642);
+            this.MinimumSize = new System.Drawing.Size(555, 642);
+            this.Name = "Form1";
+            this.Text = "2048 by leejuen";
+            this.KeyPreview = true;                                     //打开键盘监听
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);  //添加委托事件
+            ((System.ComponentModel.ISupportInitialize)(this.pb16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ini_bton;
+        private System.Windows.Forms.Label backlabel;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb12;
+        private System.Windows.Forms.PictureBox pb11;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb16;
+        private System.Windows.Forms.PictureBox pb15;
+        private System.Windows.Forms.PictureBox pb14;
+        private System.Windows.Forms.PictureBox pb13;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Label scorebord;
+
+
+    }
+}
+
